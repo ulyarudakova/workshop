9239eca906fe6e4e0d3e31c1b88571b66d7cde8e
ulyarudakova <124414189+ulyarudakova@users.noreply.github.com>
2023-03-12 14:31:23 +0700

    Update task_1_csv.py

--- task_1_csv.py
+++ task_1_csv.py
@@ -3,7 +3,7 @@ import csv
 with open('stage3_test.csv', 'r', encoding='utf-8') as f:
     with open('task_1.csv', 'w', encoding='utf-8') as file:
         reader = csv.DictReader(f)
-        writer = csv.DictWriter(file, fieldnames=["Id", "Images", "Title", "Description", "Price"], lineterminator="\r")
+        writer = csv.DictWriter(file, fieldnames=reader.fieldnames, lineterminator="\r")
         for row in reader:
             rows = row['Images']
             if len(rows.split(',')) > 3:
