e5e089aa822f9a71cd8b739d7a1e99cd5bb2aab0
ulyarudakova <124414189+ulyarudakova@users.noreply.github.com>
2023-05-31 19:43:07 +0700

    Update unittest_1.py

--- unittest_1.py
+++ unittest_1.py
@@ -1,37 +1,46 @@
 import unittest
 
 
-class FunctionsTest(unittest.TestCase):
-    def test_is_even(self):
-        num = 2
-        self.assertTrue(num % 2 == 0)
+def subtract(a, b):
+    c = a - b
+    return c < a
 
-    def test_is_odd(self):
-        num = 3
-        self.assertFalse(num % 2 == 0)
 
-    def test_is_in_list(self):
-        my_list = [1, 2, 3, 4]
-        self.assertIn(3, my_list)
+def check_list(lst, item):
+    return item in lst
 
-    def test_is_not_in_list(self):
-        my_list = [1, 2, 3, 4]
-        self.assertNotIn(5, my_list)
 
-    def test_is_greater_than(self):
-        num1 = 5
-        num2 = 3
-        self.assertGreater(num1, num2)
+def check_greater(a, b):
+    return a > b
 
-    def test_is_less_than(self):
-        num1 = 3
-        num2 = 5
-        self.assertLess(num1, num2)
 
-    def test_count_equal(self):
-        list1 = [1, 2, 3]
-        list2 = [3, 2, 1]
-        self.assertCountEqual(list1, list2)
+def check_less(a, b):
+    return a < b
+
+
+def check_count(lst, items):
+    return lst.count(items[0]) == items.count(items[0]) and lst.count(items[1]) == items.count(items[1])
+
+
+class TestFunctions(unittest.TestCase):
+    def test_subtract(self):
+        self.assertTrue(subtract(10, 3))
+        self.assertFalse(subtract(-5, -3))
+
+    def test_check_list(self):
+        lst = [1, 2, 3, 4]
+        self.assertIn(2, lst)
+        self.assertNotIn(5, lst)
+
+    def test_check_greater(self):
+        self.assertGreater(10, 5)
+
+    def test_check_less(self):
+        self.assertLess(5, 10)
+
+    def test_check_count(self):
+        lst = [1, 2, 3, 4, 2, 3]
+        self.assertCountEqual(lst, [1, 2, 2, 3, 3, 4])
 
 
 if __name__ == '__main__':
